4784a07db6215e55920eebd6f24a3798b416fdad
Mahmoud Ayman <mahmoudayman1542@gmail.com>
2024-04-27 16:37:57 +0300

    add document and query middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true
   },
+  slug: String,
   duration: {
     type: Number,
     required: [true, 'A tour must have a duration']
@@ -45,20 +47,50 @@ const tourSchema = new mongoose.Schema({
   },
   images: [String],
   startDates: [Date],
-} , {
-  toJSON : {virtuals : true},
-  toObject : {virtuals : true}
+  secretTour: {
+    type: Boolean,
+    default: false
+  }
+}, {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
 });
 
 
-  // virtual properties :  fields defined to schema but not persisted (not be saved into database) in order to save space
-  // fields to convert or calculated data
-  // can't use it in query because this actually not part of database
-tourSchema.virtual('durationWeeks').get(function (){
+// virtual properties :  fields defined to schema but not persisted (not be saved into database) in order to save space
+// fields to convert or calculated data
+// can't use it in query because this actually not part of database
+tourSchema.virtual('durationWeeks').get(function() {
   // regular function and not an arrow function because we want this keyword to point to current document
   return this.duration / 7; // calculate days and convert it into weeks
 });
 
+// Document middleware : runs before.save() and .create
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+// executed after all middleware function executed
+tourSchema.post('save', function(doc, next) {
+  console.log(doc);
+  next();
+});
+
+// Query Middleware
+// see tours for VIP Users
+// any query begin with find : find , findOne , findOneAndDelete ....
+// to prevent secret Tours from displayed in any query
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/ , function(docs , next){
+  // console.log(`Query took ${Date.now() - this.start} MS`);
+  console.log(docs);
+  next();
+})
 
 const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
