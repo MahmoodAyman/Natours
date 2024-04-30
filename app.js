be2323093ba6678703e628b96e92bdacdec558c9
Mahmoud Ayman <mahmoudayman1542@gmail.com>
2024-04-30 16:01:30 +0300

    add err handling in a seprated file

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ if (process.env.NODE_ENV === 'development') {
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
 // create middleware
 app.use(express.json());
 
@@ -35,5 +38,17 @@ app.use('/api/v1/tours', tourRouter);
 // --------------- Routes for Users ---------------
 app.use('/api/v1/users', userRouter);
 
+// --------------- function to handle all unhandled routes ---------------
+app.all('*', (request, response, next) => {
+  // const err = new Error(`can't find ${request.originalUrl} on this server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  next(new AppError(`can't find ${request.originalUrl} on this serve` , 404));
+});
+
+// --------------- Error Handling middleware ---------------
+app.use(globalErrorHandler);
+
 // Server
 module.exports = app;
+
